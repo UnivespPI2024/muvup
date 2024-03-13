c746b61f036320933cbcbd9aef8164351af177d9
Tiago Toledo <pi.muvuo@gmail.com>
2024-03-13 16:04:22 -0300

    implementado Seleção de dia e horário de aulas v2

--- src/componentes/CadastroAluno.js
+++ src/componentes/CadastroAluno.js
@@ -4,7 +4,7 @@ import { db } from '../firebase'
 import { setDoc, doc } from 'firebase/firestore/lite';
 
 const CadastroAluno = () => {
-  const [selectedOption, setSelectedOption] = useState('');
+  const [qntAulas, setQntAulas] = useState('');
 
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
@@ -31,67 +31,94 @@ const CadastroAluno = () => {
   };
 
   // seleção qnt de aulas
-  const handleSelectChange = (event) => {
-    setSelectedOption(event.target.value);
+  const handleSelectQntAulas = (event) => {
+    setQntAulas(event.target.value);
   };
 
-  return(
+
+  return (
     <div style={styles.cadastroContainer}>
       <h2 style={styles.texto}>Cadastro de Aluno</h2>
-            <div style={styles.formGroup}>
-              <input
-                type="text"
-                placeholder="Nome"
-                value={nome}
-                onChange={(e) => setNome(e.target.value)}
-              />
-            </div>
-  
-            <div style={styles.formGroup}>
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div style={styles.formGroup}>
-              <input
-                type="text"
-                placeholder="Telefone"
-                value={telefone}
-                onChange={(e) => setTelefone(e.target.value)}
-              />
-            </div>
-            <div style={styles.formGroup}>
-              <input
-                type="text"
-                placeholder="Endereço"
-                value={endereco}
-                onChange={(e) => setEndereco(e.target.value)}
-              />
-            </div>
-            <div style={styles.formGroup}>
-              <input
-                type="text"
-                placeholder="Cidade"
-                value={cidade}
-                onChange={(e) => setCidade(e.target.value)}
-              />
-            </div>
-            <div>
-              <select
-                style={styles.select}
-                value={selectedOption}
-                onChange={handleSelectChange}>
-                <option value="">Quantidade de aulas na semana</option>
-                <option value="1aula">1 aula</option>
-                <option value="2aulas">2 aulas</option>
-                <option value="3aulas">3 aulas</option>
-              </select>
-            </div>
-            <SelHorAulaAluno></SelHorAulaAluno>
-            <button style={styles.btnCadastrar} onClick={handleCadastro}>Cadastrar Aluno</button>
+      <div style={styles.formGroup}>
+        <input
+          type="text"
+          placeholder="Nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+      </div>
+
+      <div style={styles.formGroup}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <input
+          type="text"
+          placeholder="Telefone"
+          value={telefone}
+          onChange={(e) => setTelefone(e.target.value)}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <input
+          type="text"
+          placeholder="Endereço"
+          value={endereco}
+          onChange={(e) => setEndereco(e.target.value)}
+        />
+      </div>
+      <div style={styles.formGroup}>
+        <input
+          type="text"
+          placeholder="Cidade"
+          value={cidade}
+          onChange={(e) => setCidade(e.target.value)}
+        />
+      </div>
+      <div>
+        <select
+          style={styles.select}
+          value={qntAulas}
+          onChange={handleSelectQntAulas}>
+          <option value="">Quantidade de aulas na semana</option>
+          <option value="1aula">1 aula</option>
+          <option value="2aulas">2 aulas</option>
+          <option value="3aulas">3 aulas</option>
+        </select>
+      </div>
+      {
+        qntAulas=='1aula'?
+        <div>
+          <text>Selecione dia e horário da primeira aula</text>
+          <SelHorAulaAluno/>
+        </div>:null
+      }
+      {
+        qntAulas=='2aulas'?
+        <div>
+          <text>Selecione dia e horário da primeira aula</text>
+          <SelHorAulaAluno/>
+          <text>Selecione dia e horário da segunda aula</text>
+          <SelHorAulaAluno/>
+        </div>:null
+      }
+      {
+        qntAulas=='3aulas'?
+        <div>
+          <text>Selecione dia e horário da primeira aula</text>
+          <SelHorAulaAluno/>
+          <text>Selecione dia e horário da segunda aula</text>
+          <SelHorAulaAluno/>
+          <text>Selecione dia e horário da terceira aula</text>
+          <SelHorAulaAluno/>
+        </div>:null
+      }
+      <button style={styles.btnCadastrar} onClick={handleCadastro}>Cadastrar Aluno</button>
     </div>
   )
 
